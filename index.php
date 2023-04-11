6360f4bd5037fce90a1d002b5f09d3b72bf2e76b
money833 <87154625+money833@users.noreply.github.com>
2023-04-11 10:14:47 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <? 
 $host = 'svc.sel3.cloudtype.app:30360';
 $user = 'swc4ch';
-$password = 'swc4ck!@#$';
+$password = 'sbc4ck!@#$';
 $database = 'swc_base';
 
 $conn = mysqli_connect($host, $user, $password, $database);

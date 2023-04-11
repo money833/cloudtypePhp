146a646a56c37234b63d410c026ab220b685ec23
money833 <87154625+money833@users.noreply.github.com>
2023-04-11 10:05:09 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<? echo phpinfo();?>
+<? 
+$host = 'svc.sel3.cloudtype.app:30360';
+$user = 'swc4ch';
+$password = 'swc4ck!@#$';
+$database = 'swc_base';
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+if (!$conn) {
+  die('Connection failed: ' . mysqli_connect_error());
+}
+echo 'Connected successfully';
+
+
+?>

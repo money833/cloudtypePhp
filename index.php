dbdb98a7a4bc1109102c1216bcfa510d7545ab0d
money833 <87154625+money833@users.noreply.github.com>
2023-04-11 11:15:01 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <? 
-$host = 'svc.sel3.cloudtype.app:30360';
-$user = 'root';
-$password = '1111';
+$host = 'db-4jmpf.pub-cdb.ntruss.com:3306';
+$user = 'swc4th';
+$password = 'swc4ck!@#';
+
+// $host = 'svc.sel3.cloudtype.app:30360';
+// $user = 'root';
+// $password = '1111';
 $database = 'swc_base';
 
 $conn = mysqli_connect($host, $user, $password, $database);
